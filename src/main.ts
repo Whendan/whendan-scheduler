7c3d287a89c6b7f856b62d52efc9cf8946a701bf
Daniel Quah <danielquah148@yahoo.com>
2026-05-07 09:18:52 +1000

    Added swagger module

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,28 @@ import {
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  
+  const config = new DocumentBuilder()
+    .setTitle('Whendan Logistics API')
+    .setDescription('The Whendan Logistics API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
   await app.listen(process.env.PORT ?? 3002, '0.0.0.0');
 }
 bootstrap();
